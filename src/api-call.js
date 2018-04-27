37133de61c3276d6ab2f97aab7eea72b9ea9e7d2
Garnett Puett <gpuett@gmail.com>
2018-04-27 08:43:11 -0700

    begin user interface logic.

--- src/api-call.js
+++ src/api-call.js
@@ -3,3 +3,5 @@ class API {
     return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&query=${symptom}&location=or-portland&user_location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`)
   }
 }
+
+export { API };
--- src/main.js
+++ src/main.js
@@ -5,5 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-
+  $('#query').submit(function(event) {
+    event.preventDefault();
+    let symptom = $('#symptom').val();
+    $('#symptom').val("");
+    let doctor = $('#doctor').val();
+    $('#doctor').val("");
+    let api = new API();
+    
+  });
 });
