6565865c47efe4a4d6e7229d969cfce29997258f
Garnett Puett <gpuett@gmail.com>
2018-04-27 11:07:20 -0700

    Add background and css styling.

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $('.results').text(`Your query found no results.`)
       } else {
         for (let i=0; i<response.data.length; i++) {
-        $('.results').append(`<li><strong>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</strong> <br> <strong>Address:</strong><br> ${response.data[i].practices[0].visit_address.street} <br> ${response.data[i].practices[0].visit_address.city} ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip} <br> <strong> Phone:</strong><br> ${response.data[i].practices[0].phones[0].number} <br> <strong>Accepting New Patients:</strong> ${response.data[i].practices[0].accepts_new_patients} </li>`);
+        $('.results').append(`<li><strong>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</strong> <br> <strong>Address:</strong><br> ${response.data[i].practices[0].visit_address.street} <br> ${response.data[i].practices[0].visit_address.city} ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip} <br> <strong> Phone:</strong><br> ${response.data[i].practices[0].phones[0].number} <br> <strong>Accepting New Patients:</strong> ${response.data[i].practices[0].accepts_new_patients} </li><br>`);
         }
       }
     }).fail(function(error) {
