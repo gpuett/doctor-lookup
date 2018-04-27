61e633e11d3b900b251721e594069d613290f006
Garnett Puett <gpuett@gmail.com>
2018-04-27 08:59:18 -0700

    write inital result and error messages.

--- src/main.js
+++ src/main.js
@@ -7,11 +7,16 @@ import './styles.css';
 $(document).ready(function() {
   $('#query').submit(function(event) {
     event.preventDefault();
-    let symptom = $('#symptom').val();
-    $('#symptom').val("");
     let doctor = $('#doctor').val();
     $('#doctor').val("");
+    let symptom = $('#symptom').val();
+    $('#symptom').val("");
     let api = new API();
-    
+    let list = api.call(doctor, symptom);
+    list.then(function(response) {
+      $('.results').text(`${response.data.practices.profile.slug}`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
   });
 });
