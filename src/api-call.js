2db16373a21ce2e7fa7d33bfc061e85a71cc09b1
Garnett Puett <gpuett@gmail.com>
2018-04-27 08:39:46 -0700

    Add business logic for api call.

---
+++ src/api-call.js
@@ -0,0 +1,5 @@
+class API {
+  call(doctor, symptom) {
+    return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&query=${symptom}&location=or-portland&user_location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`)
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// import { Constructor } from './project.js';
+import { API } from './api-call.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  
+
 });
